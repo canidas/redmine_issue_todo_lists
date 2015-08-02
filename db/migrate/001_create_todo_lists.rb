78034bb918cca2e0624f96319a631511d7866264
Den <dev@den.cx>
2015-08-02 15:15:51 +0200

    Renamed plugin directory

--- db/migrate/001_create_todo_lists.rb
+++ db/migrate/001_create_todo_lists.rb
@@ -1,5 +1,20 @@
 class CreateTodoLists < ActiveRecord::Migration
-  def change
+  def up
+    # Check if migration has already been run
+    version = ::ActiveRecord::Base.connection.select_values(
+        "SELECT version FROM #{::ActiveRecord::Migrator.schema_migrations_table_name} WHERE version='1-issue_todo_lists'"
+    )
+
+    # Plugin directory has been renamed
+    # Delete entry and return because migration adds entry afterwards anyway
+    if version.length != 0
+      execute <<-SQL
+        DELETE FROM #{::ActiveRecord::Migrator.schema_migrations_table_name} WHERE version = '1-issue_todo_lists';
+      SQL
+
+      return
+    end
+
     create_table :issue_todo_lists do |t|
       t.string :project_identifier
       t.string :title
@@ -14,5 +29,10 @@ class CreateTodoLists < ActiveRecord::Migration
       t.integer :issue_id
       t.integer :position
     end
+  end
+
+  def down
+    drop_table :issue_todo_lists
+    drop_table :issue_todo_list_items
   end
 end
--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
-Redmine::Plugin.register :issue_todo_lists do
+Redmine::Plugin.register :redmine_issue_todo_lists do
   name 'Issue To-do Lists Plugin'
   author 'Den'
   description 'Organize issues in to-do lists by manually ordering their priority'
   version '1.0.1'
-  url 'https://github.com/Canidas/issue_todo_lists'
+  url 'https://github.com/Canidas/redmine_issue_todo_lists'
   author_url 'http://den.cx'
 
   project_module :issue_todo_lists do
